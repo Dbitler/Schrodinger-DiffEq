f95a3341fdfcc9f5d1937a48fbc3dd13e1bbaec6
Dbitler <dbitler@hawk.iit.edu>
2023-03-10 17:13:35 -0600

    Initial Commit

---
+++ Schrodinger DiffEq/Schrodinger_DiffEqApp.swift
@@ -0,0 +1,17 @@
+//
+//  Schrodinger_DiffEqApp.swift
+//  Schrodinger DiffEq
+//
+//  Created by IIT PHYS 440 on 3/10/23.
+//
+
+import SwiftUI
+
+@main
+struct Schrodinger_DiffEqApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
